42a2585dd7e0908ef9bab3ac8155446c7d3995f5
GTU9 <sangik9999@naver.com>
2024-03-25 13:46:13 +0900

    Start, Student, Application, Ending 수정사항 #33

--- src/main/java/com/SYproject/Start.java
+++ src/main/java/com/SYproject/Start.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 
 public class Start {
     public void start (Student player){
-        System.out.print("닉네임을 입력해주세요: ");
-        Scanner sc = new Scanner(System.in);
-        String name = sc.next();
-        EventTxt eventTxt = new EventTxt();
-
-            // 캐릭터 생성
-            int day =1;  // 14일간 게임 진행
-            System.out.println("Day " + day); // 날짜 출력
-
-            // 캐릭터 상태 출력
-            if (day % 4 == 0) {
-                eventTxt.displayStats(player);
-            }
+//        System.out.print("닉네임을 입력해주세요: ");
+//        Scanner sc = new Scanner(System.in);
+//        String name = sc.next();
+//        EventTxt eventTxt = new EventTxt();
+//
+//            // 캐릭터 생성
+//            int day =1;  // 14일간 게임 진행
+//            System.out.println("Day " + day); // 날짜 출력
+//
+//            // 캐릭터 상태 출력
+//            if (day % 4 == 0) {
+//                eventTxt.displayStats(player);
+//            }
 
             // 아침 이벤트 실행
             Morning morning = new Morning();
