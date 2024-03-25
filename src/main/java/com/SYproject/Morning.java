7525e5d001306b9c5a4ac83ab09c96eabb68ee1d
Baeminseo <qoalstj01@naver.com>
2024-03-25 14:41:40 +0900

    오전, 오후 고정이벤트 수정

--- src/main/java/com/SYproject/Morning.java
+++ src/main/java/com/SYproject/Morning.java
@@ -11,8 +11,8 @@ public class Morning {
         RandomEvent randomEvent = new RandomEvent();
 
         // 시간 설정 및 체력 설정
-        int hp = 30;
-        player.setHp(hp);
+//        int hp = 30;
+//        player.setHp(hp);
 
         // 아침기상 이후 이벤트 출력
         randomEvent.morningRandomEvent();
@@ -22,16 +22,21 @@ public class Morning {
         int choice = sc.nextInt();
         switch (choice) {
             case 1:
-                randomEvent.randomEventWalkingAction(hp);
+                //걸어가는 거
+                randomEvent.randomEventWalkingAction(player);
+
                 break;
             case 2:
-                randomEvent.randomEventBusAction(hp);
+                //버스 타는거
+                randomEvent.randomEventBusAction(player);
                 break;
             case 3:
-                randomEvent.randomEventSubwayAction(hp);
+                //전철 타는 거
+                randomEvent.randomEventSubwayAction(player);
                 break;
             case 4:
-                randomEvent.randomEventTaxiAction(hp);
+                //택시 타는 거
+                randomEvent.randomEventTaxiAction(player);
                 break;
         }
         morningSchoolEvent(player);
@@ -46,18 +51,22 @@ public class Morning {
         // 체력이 6 미만인 경우
         if (player.getHp() < 6) {
             System.out.println("체력이 부족하여 잠들었습니다.");
+            System.out.println("[체력] : "+player.getHp()+" [소지금] : "+player.getMoney()+" [지능] : "+player.getIntelligence()+" [인망] : "+player.getFame());
             sc.close();
             return; // 메서드 종료
         }
         // 오전 수업 중 돌발 이벤트 발생
         start.morningClassRandomEvent();
 
+        System.out.println();
         System.out.println("오전 수업이 다가왔습니다!!");
+        System.out.println("수업을 어떻게 들을까요?");
         for (String event : eventTxt.morningStudyFEvent) {
             System.out.println(event);
         }
         System.out.print("할 일을 선택해주세요~ : ");
         int choice = sc.nextInt();
+        System.out.println();
 
         // 문제를 푸는 경우 (2번 선택)
         if (choice == 2) {
@@ -100,18 +109,26 @@ public class Morning {
                     // 체력이 10 이상이면 지능을 5 증가
                     if (player.getHp() >= 10) {
                         player.setIntelligence(player.getIntelligence() + 5);
+                        System.out.println("[체력] : "+player.getHp()+" [소지금] : "+player.getMoney()+" [지능] : "+player.getIntelligence()+" [인망] : "+player.getFame());
+
                     } else { // 체력이 6 이상 10 미만인 경우 지능을 4 증가
                         player.setIntelligence(player.getIntelligence() + 4);
+                        System.out.println("[체력] : "+player.getHp()+" [소지금] : "+player.getMoney()+" [지능] : "+player.getIntelligence()+" [인망] : "+player.getFame());
+
                     }
                 }
             } else {
                 System.out.println("오답입니다. 공부를 좀 더 해주세요.");
+                System.out.println("[체력] : "+player.getHp()+" [소지금] : "+player.getMoney()+" [지능] : "+player.getIntelligence()+" [인망] : "+player.getFame());
+
             }
         } else if (choice == 1){
             System.out.println("흠냐흠냐 잘잤다..");
             // 체력과 지능 감소
             player.setHp(player.getHp() - 2);
             player.setIntelligence(player.getIntelligence() - 2);
+            System.out.println("[체력] : "+player.getHp()+" [소지금] : "+player.getMoney()+" [지능] : "+player.getIntelligence()+" [인망] : "+player.getFame());
+
         }
         else if (choice == 3) {
             // 50%의 확률로 정글님 사랑해요 메시지 출력
@@ -119,16 +136,22 @@ public class Morning {
                 System.out.println("정글님 사랑해요!!!!!!!!! ... 승급에 성공했다.");
                 player.setHp(player.getHp() + 5); // 체력 증가
                 player.setIntelligence(player.getIntelligence() + 5); // 지능 증가
+                System.out.println("[체력] : "+player.getHp()+" [소지금] : "+player.getMoney()+" [지능] : "+player.getIntelligence()+" [인망] : "+player.getFame());
+                System.out.println();
             } else {
                 System.out.println("아 우리 서포터 왜 이래!!! ... 강등 당했다.");
                 player.setHp(player.getHp() - 2); // 체력 감소
                 player.setIntelligence(player.getIntelligence() - 2); // 지능 감소
+                System.out.println("[체력] : "+player.getHp()+" [소지금] : "+player.getMoney()+" [지능] : "+player.getIntelligence()+" [인망] : "+player.getFame());
+                System.out.println();
             }
         } else if (choice==4) {
             System.out.println("몰폰하냐고 죽는줄 알았네..");
             // 체력과 지능 감소
             player.setHp(player.getHp() - 2);
             player.setIntelligence(player.getIntelligence() - 2);
+            System.out.println("[체력] : "+player.getHp()+" [소지금] : "+player.getMoney()+" [지능] : "+player.getIntelligence()+" [인망] : "+player.getFame());
+            System.out.println();
         }
 
 
