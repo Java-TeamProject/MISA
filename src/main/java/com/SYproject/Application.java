b4bc3f858f2ff37d76868c71982df1f99006928e
ohdyo <smart5572@naver.com>
2024-03-26 22:29:50 +0900

    Merge main Application to prototype Application

--- src/main/java/com/SYproject/Application.java
+++ src/main/java/com/SYproject/Application.java
@@ -9,6 +9,10 @@ public class Application {
         Student student = new Student(100, 0, 0, 0);
         System.out.print("이름을 입력하세요 : ");
         student.setName(sc.nextLine());
+        System.out.println("반갑습니다. " + student.getName() + "님!");
+        System.out.println("당신은 지금 마지막 학기만 남겨둔 4학년 2학기 취준생입니다.");
+        System.out.println("마지막 학교 생활을 시작하면서 다양한 상황들과 선택들이 당신을 기다리고 있습니다.");
+        System.out.println("사이버 삼육대학교에서 당신의 미래를 펼쳐보세요.");
 
 
         while(true) {
@@ -24,7 +28,7 @@ public class Application {
             AfterFix afterFix = new AfterFix();
             afterFix.AfterFixEvent(student);
             day++;
-            if(day>3)break;
+            if(day>10)break;
         }
 
         Ending ending = new Ending(student);
